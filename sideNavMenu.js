be9d03e4b878a06d48b58c1535575af3ef2f2dee
CalosGLOZ <cgiraldolozano@gmail.com>
2021-10-30 00:09:35 +0200

    fixed and styled menus for all pages

--- sideNavMenu.js
+++ sideNavMenu.js
@@ -2,10 +2,13 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("body").style.marginLeft = "250px";
+    document.getElementById("menu-icon").style.marginLeft = "0.7%";
+    document.getElementById("menu-icon").style.display = "none";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("body").style.marginLeft = "2%";
+    document.getElementById("body").style.marginLeft = "0.6%";
+    document.getElementById("menu-icon").style.display = "initial";
 }
